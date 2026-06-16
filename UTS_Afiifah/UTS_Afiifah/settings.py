aa4e1059c35d7811fe9a23cf34a1ca5d9ce89173
pcrsunghoon <afifahkhoirunnisa106@gmail.com>
2026-06-16 17:37:05 +0700

    Update settings.py

--- UTS_Afiifah/UTS_Afiifah/settings.py
+++ UTS_Afiifah/UTS_Afiifah/settings.py
@@ -41,7 +41,6 @@ INSTALLED_APPS = [
 
 MIDDLEWARE = [
     'django.middleware.security.SecurityMiddleware',
-    'whitenoise.middleware.WhiteNoiseMiddleware',  # <-- Baris baru yang disisipkan di sini
     'django.contrib.sessions.middleware.SessionMiddleware',
     'django.middleware.common.CommonMiddleware',
     'django.middleware.csrf.CsrfViewMiddleware',
